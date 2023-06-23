3ebf325a6b1523b092af0f5a29a8b2cbc9d20e0e
Oumaima Fisaoui <oufisaou@e1r3p10.1337.ma>
2023-06-23 20:34:03 +0100

    add home

---
+++ components/SignIn.tsx
@@ -0,0 +1,21 @@
+
+
+const Signin = () =>
+{
+    return (  
+        <section id="signin" className="container bg-black/20 ml-40 mr-40">
+            <div >
+            <p >Sign In</p>
+            <div>
+                <button>
+                Sign In with Intra
+                </button>
+                <p>or</p>
+                {/* <LoginForm /> */}
+            </div>
+            </div>
+        </section>
+    );
+}
+
+export default Signin;
